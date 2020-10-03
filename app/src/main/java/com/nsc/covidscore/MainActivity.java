305aca8046476009c8ce9e5f57b6a71117b8b38c
dsjack <dsjack89@gmail.com>
2020-10-03 01:08:26 -0700

    Add volley and make request to novel covid api

--- app/src/main/java/com/nsc/covidscore/MainActivity.java
+++ app/src/main/java/com/nsc/covidscore/MainActivity.java
@@ -3,12 +3,46 @@ package com.nsc.covidscore;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        restTest("King");
+    }
+
+    private void restTest(String county) {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://corona.lmao.ninja/v2/jhucsse/counties/" + county;
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.i(TAG, "onResponse: " + response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.i(TAG, "onError: That didn't work!" + error);
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
+
     }
 }
