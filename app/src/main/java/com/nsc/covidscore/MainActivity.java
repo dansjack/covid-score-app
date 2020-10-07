d5a2b66bb999ac751905fedcc8669004862ee1d9
dsjack <dsjack89@gmail.com>
2020-10-07 16:00:41 -0700

    Remove requests from mainactivity

--- app/src/main/java/com/nsc/covidscore/MainActivity.java
+++ app/src/main/java/com/nsc/covidscore/MainActivity.java
@@ -4,21 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.TextView;
 
 import com.android.volley.RequestQueue;
 import com.nsc.covidscore.api.RequestSingleton;
-import com.nsc.covidscore.api.Requests;
-import com.nsc.covidscore.api.VolleyJsonCallback;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     RequestQueue queue;
     RequestSingleton requestManager;
-    TextView textView;
 
 
     @Override
@@ -27,33 +20,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         requestManager = RequestSingleton.getInstance(this.getApplicationContext());
         queue = requestManager.getRequestQueue();
-        textView = findViewById(R.id.hello_world);
-        Requests.getCounty(this, "king,washington", new VolleyJsonCallback() {
-            @Override
-            public void getJsonData(JSONObject response) throws JSONException {
-                Log.i(TAG, "getCounty : " + response);
-            }
-
-            @Override
-            public void getJsonException(Exception exception) {
-                Log.i(TAG, "getJsonException: " + exception.getMessage());
-            }
-        });
-        Requests.getCountyHistorical(this, "whatcom,washington", "30", new VolleyJsonCallback() {
-            @Override
-            public void getJsonData(JSONObject response) throws JSONException {
-                Log.i(TAG, "getCountyHistorical : " + response);
-//                JSONObject timeline = response.getJSONObject("timeline");
-//                JSONObject cases = timeline.getJSONObject("cases");
-//                JSONObject deaths = timeline.getJSONObject("deaths");
-//                Log.i(TAG, "getCountyHistorical - cases, last 30 days - Whatcom, WA: " + cases);
-            }
-
-            @Override
-            public void getJsonException(Exception exception) {
-                Log.i(TAG, "getJsonException: " + exception.getMessage());
-            }
-        });
         Log.d(TAG,"onCreate invoked");
     }
 
