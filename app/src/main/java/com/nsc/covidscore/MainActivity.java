b5e15d2ecca9b5792342a9b03174fac26e98948d
dsjack <dsjack89@gmail.com>
2020-10-07 18:25:14 -0700

    re-add calls to main

--- app/src/main/java/com/nsc/covidscore/MainActivity.java
+++ app/src/main/java/com/nsc/covidscore/MainActivity.java
@@ -7,6 +7,11 @@ import android.util.Log;
 
 import com.android.volley.RequestQueue;
 import com.nsc.covidscore.api.RequestSingleton;
+import com.nsc.covidscore.api.Requests;
+import com.nsc.covidscore.api.VolleyJsonCallback;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
@@ -20,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         requestManager = RequestSingleton.getInstance(this.getApplicationContext());
         queue = requestManager.getRequestQueue();
+        makeApiCalls();
         Log.d(TAG,"onCreate invoked");
     }
 
@@ -29,5 +35,36 @@ public class MainActivity extends AppCompatActivity {
         if (queue != null) {
             requestManager.getRequestQueue().cancelAll(TAG);
         }
+    }
+
+    private void makeApiCalls() {
+        Requests.getCounty(this, "king,washington", new VolleyJsonCallback() {
+            @Override
+            public void getJsonData(JSONObject response) {
+            }
+
+            @Override
+            public void getJsonException(Exception exception) {
+            }
+        });
+        Requests.getCountyHistorical(this, "whatcom,washington", "30", new VolleyJsonCallback() {
+            @Override
+            public void getJsonData(JSONObject response) {
+            }
+
+            @Override
+            public void getJsonException(Exception exception) {
+            }
+        });
+        Requests.getUSHistorical(this, "1", new VolleyJsonCallback() {
+            @Override
+            public void getJsonData(JSONObject response) {
+            }
+
+            @Override
+            public void getJsonException(Exception exception) {
+
+            }
+        });
     }
 }
--- app/src/test/java/com/nsc/covidscore/VolleyJsonCallbackUnitTests.java
+++ app/src/test/java/com/nsc/covidscore/VolleyJsonCallbackUnitTests.java
@@ -10,13 +10,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.internal.verification.VerificationModeFactory.times;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
-public class VolleyJsonCallbackUnitTestS {
-
+public class VolleyJsonCallbackUnitTests {
     @Test
     public void getJsonData_invoked() throws JSONException {
        final VolleyJsonCallback callback = mock(VolleyJsonCallback.class);
