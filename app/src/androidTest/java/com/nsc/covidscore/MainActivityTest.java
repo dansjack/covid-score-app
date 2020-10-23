8184d57321e1da5bdbda126107cbe45f61df9f72
unknown <lyaholkovsky@gmail.com>
2020-10-23 14:09:40 -0700

    added testing

--- app/src/androidTest/java/com/nsc/covidscore/MainActivityTest.java
+++ app/src/androidTest/java/com/nsc/covidscore/MainActivityTest.java
@@ -17,10 +17,7 @@ import java.util.Date;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.espresso.matcher.ViewMatchers.*;
 import static org.junit.Assert.*;
 
 @RunWith(AndroidJUnit4.class)
@@ -34,5 +31,16 @@ public class MainActivityTest {
     public void loadsViews() {
         onView(withId(R.id.hello_world))
                 .check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testCalculation() {
+        // King County 10/23/20
+        assertEquals(RiskCalculation.calculateRisk(24914, 2252782, 20), 19.94, 0);
+        // WA State 10/23/20
+        assertEquals(RiskCalculation.calculateRisk(52948, 7614893, 20), 13.03, 0);
+        // US 10/23/20
+        assertEquals(RiskCalculation.calculateRisk(4831614, 328239523, 20), 25.66, 0);
+
     }
 }
