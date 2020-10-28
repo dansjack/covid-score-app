fd40966184bfb3bbf2443cd67bb83168bd775c38
dsjack <dsjack89@gmail.com>
2020-10-27 17:27:56 -0700

    Revert "Merge remote-tracking branch 'origin/work/room-api-ui' into ec-issue-30"
    This reverts commit 8f9a51841b2ce7c3f79bea54eebc3f5be4a41ae0, reversing
    changes made to 43bc36a102c7d636de0190fa9cbbeb7656437ee5.

--- app/src/androidTest/java/com/nsc/covidscore/MainActivityTest.java
+++ app/src/androidTest/java/com/nsc/covidscore/MainActivityTest.java
@@ -1,17 +1,37 @@
 package com.nsc.covidscore;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
-import static org.junit.Assert.assertEquals;
+import java.util.Date;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.*;
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
 
     @Rule
     public ActivityTestRule<MainActivity> activityTestRule
             = new ActivityTestRule<>(MainActivity.class, true, true);
+
+    @Test
+    public void loadsViews() {
+        onView(withId(R.id.hello_world))
+                .check(matches(isDisplayed()));
+    }
 
     @Test
     public void testCalculation() {
