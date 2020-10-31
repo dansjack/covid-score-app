f3c0f10b501ad14dbb489109d4b01c33d0406af0
dsjack <dsjack89@gmail.com>
2020-10-30 22:16:21 -0700

    Adds CovidSnapshot location test

--- app/src/test/java/com/nsc/covidscore/LocationUnitTest.java
+++ app/src/test/java/com/nsc/covidscore/LocationUnitTest.java
@@ -7,7 +7,6 @@ import org.junit.Test;
 
 import java.util.Calendar;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
