8225523c4470a4a7e05c021caa0b49859854e19c
unknown <lyaholkovsky@gmail.com>
2020-11-10 13:02:46 -0800

    reworked tests

--- app/src/androidTest/java/com/nsc/covidscore/MainActivityTest.java
+++ app/src/androidTest/java/com/nsc/covidscore/MainActivityTest.java
@@ -1,6 +1,7 @@
 package com.nsc.covidscore;
 
 import androidx.test.espresso.NoMatchingViewException;
+import androidx.test.espresso.PerformException;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
@@ -33,8 +34,18 @@ public class MainActivityTest {
             new ActivityScenarioRule<>(MainActivity.class);
 
     @Test
-    public void t1_canSelectLocation() throws InterruptedException {
+    public void t1_canSelectLocationAndViewRisk() throws InterruptedException {
         try {
+            Thread.sleep(8000); // Time for app to load
+            try { // Are we in the LocationManualSelectionFragment?
+                // select state
+                onView(withId(R.id.state_spinner)).perform(click());
+                onData(allOf(is(instanceOf(String.class)), is("California"))).perform(click());
+            } catch (NoMatchingViewException | PerformException ex) {
+                // We are in RiskDetailFragment - click select new
+                onView(withId(R.id.select_location_btn)).perform(click());
+            }
+
             // inside the LocationManualSelectionFragment
 
             // select state
@@ -50,38 +61,86 @@ public class MainActivityTest {
 
             Thread.sleep(5000);
             onView(withId(R.layout.fragment_location_selection)).check(matches(isDisplayed()));
-        } catch (NoMatchingViewException ex) {
+        } catch (NoMatchingViewException | PerformException ex) {
             // pass over this test to avoid crashing, we're in the RiskDetailPageFragment
+            // inside the RiskDetailPageFragment
+            onView(withId(R.id.activeCounty)).check(matches(not(withText(""))));
         }
     }
 
-    @Test
-    public void t2_canViewCovidStats() {
-        // inside the RiskDetailPageFragment
-        onView(withId(R.id.activeCounty)).check(matches(not(withText(""))));
-    }
+//    @Test
+//    public void t2_canViewCovidStats() {
+//        // inside the RiskDetailPageFragment
+//        onView(withId(R.id.activeCounty)).check(matches(not(withText(""))));
+//    }
 
     @Test
-    public void t3_canSelectNewLocation() throws InterruptedException {
-        Thread.sleep(1000);
-        onView(withId(R.id.fullscreen_content_controls)).perform(swipeDown());
-        onView(withId(R.id.select_location_btn)).perform(click());
+    public void t2_canSelectNewLocation() throws InterruptedException {
+        try {
+            Thread.sleep(8000); // Time for the app to load
 
-        // click submit
-        onView(withId(R.id.submit_btn)).perform(click());
-        onView(withId(R.id.loadingTextView)).check(matches(withText("Please pick a state and county")));
+            try { // Are we in the LocationManualSelectionFragment? Get to the RiskDetailFragment
+                // select state
+                onView(withId(R.id.state_spinner)).perform(click());
+                onData(allOf(is(instanceOf(String.class)), is("California"))).perform(click());
 
-        // select state
-        onView(withId(R.id.state_spinner)).perform(click());
-        onData(allOf(is(instanceOf(String.class)), is("Washington"))).perform(click());
+                // select county
+                onView(withId(R.id.county_spinner)).perform(click());
+                onData(allOf(is(instanceOf(String.class)), is("imperial"))).perform(click());
 
-        // select county
-        onView(withId(R.id.county_spinner)).perform(click());
-        onData(allOf(is(instanceOf(String.class)), is("king"))).perform(click());
+                // click submit
+                onView(withId(R.id.submit_btn)).perform(click());
 
-        Thread.sleep(3000);
-        pressBack();
+                Thread.sleep(5000);
+                onView(withId(R.layout.fragment_location_selection)).check(matches(isDisplayed()));
+            } catch (NoMatchingViewException | PerformException ex) {
+                // We are in RiskDetailFragment - click select new
+                onView(withId(R.id.select_location_btn)).perform(click());
+            }
+            // In New LocationManualSelectionFragment
+
+            // click submit
+            onView(withId(R.id.submit_btn)).perform(click());
+            onView(withId(R.id.loadingTextView)).check(matches(withText("Please pick a state and county")));
+
+            // select state
+            onView(withId(R.id.state_spinner)).perform(click());
+            onData(allOf(is(instanceOf(String.class)), is("Washington"))).perform(click());
+
+            // select county
+            onView(withId(R.id.county_spinner)).perform(click());
+            onData(allOf(is(instanceOf(String.class)), is("king"))).perform(click());
+
+            Thread.sleep(3000);
+            pressBack();
+
+        } catch (PerformException | NoMatchingViewException ex) {
+            // we're in the RiskDetailPageFragment
+            onView(withId(R.id.activeCounty)).check(matches(not(withText(""))));
+        }
     }
+//
+//    @Test
+//    public void t3_canSelectNewLocation() throws InterruptedException {
+//        Thread.sleep(1000);
+//        onView(withId(R.id.fullscreen_content_controls)).perform(swipeDown());
+//        onView(withId(R.id.select_location_btn)).perform(click());
+//
+//        // click submit
+//        onView(withId(R.id.submit_btn)).perform(click());
+//        onView(withId(R.id.loadingTextView)).check(matches(withText("Please pick a state and county")));
+//
+//        // select state
+//        onView(withId(R.id.state_spinner)).perform(click());
+//        onData(allOf(is(instanceOf(String.class)), is("Washington"))).perform(click());
+//
+//        // select county
+//        onView(withId(R.id.county_spinner)).perform(click());
+//        onData(allOf(is(instanceOf(String.class)), is("king"))).perform(click());
+//
+//        Thread.sleep(3000);
+//        pressBack();
+//    }
 
 
 }
--- app/src/androidTest/java/com/nsc/covidscore/RoomTest.java
+++ app/src/androidTest/java/com/nsc/covidscore/RoomTest.java
@@ -24,7 +24,6 @@ import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 
 @RunWith(AndroidJUnit4.class)
 public class RoomTest {
