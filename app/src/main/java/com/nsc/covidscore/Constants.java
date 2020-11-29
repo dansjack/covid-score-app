7015654560f9ced8f10c29ff7fc218b4f1370dc5
dsjack <dsjack89@gmail.com>
2020-11-28 19:35:56 -0800

    Change how api key is imported

--- app/src/main/java/com/nsc/covidscore/Constants.java
+++ app/src/main/java/com/nsc/covidscore/Constants.java
@@ -1,7 +1,7 @@
 package com.nsc.covidscore;
 
 public class Constants {
-    public static final String CENSUS_API_KEY = "284928f69049b0901d7c04abfacf724536890594";
+    public static final String CENSUS_API_KEY = BuildConfig.CENSUS_API_KEY;
     public static final String COUNTY = "county";
     public static final String COUNTY_HISTORICAL = "countyHistorical";
     public static final String COUNTY_POPULATION = "countyPopulation";
